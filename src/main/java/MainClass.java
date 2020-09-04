92ee547da10127ff011c4a4cf332d6057bad1520
Sergei-dev-ru <sapozhnikoff1996@yandex.ru>
2020-09-04 18:45:57 +0300

    Add first\last name and result lines

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,22 +1,16 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainClass extends Application {
